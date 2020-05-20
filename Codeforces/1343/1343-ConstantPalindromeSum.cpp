2de97bfb950a0590a16e066765e708a9650928ab
Paco <paco.sanchez.ram@gmail.com>
2020-05-20 17:37:13 -0500

    update

--- Codeforces/1343/1343-ConstantPalindromeSum.cpp
+++ Codeforces/1343/1343-ConstantPalindromeSum.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
 using namespace std;
 
@@ -13,13 +12,32 @@ int main() {
     while(t--) {
         cin >> n >> k;
 
-        vector<int>a(n)
+        vector<int>a(n);
         for(auto &it: a)
             cin >> it;
-        unordered_map<int,int> cnt;
+        vector<int> sums(2*k+1);
         for(int i = 0;i < n/2; ++i) {
-            ++cnt[a[i] + a[n - i - 1]];
+            ++sums[a[i] + a[n - i - 1]];
         }
+
+        vector<int> psum(2*k+2);
+        for(int i = 0; i < n/2; ++i) {
+            int l = min(a[i], a[n - i - 1]) + 1;
+            int r = max(a[i], a[n - i - 1]) + k;
+            ++psum[l];
+            --psum[r + 1];
+        }
+
+        for(int i = 1;i <= 2*k+1; ++i) {
+            psum[i] += psum[i-1];
+        }
+
+        int ans = 1e9;
+        for(int i = 2;i <= 2*k; ++i) {
+            ans = min(ans, (psum[i] - sums[i]) + (n/2 - psum[i]) * 2);
+        }
+
+        cout << ans << endl;
     }
     return 0;
 }
